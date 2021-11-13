48129fb21dee744e6ae7a1b3260b5a00aa00ee8a
AshbeeKim <ksb.forest@gmail.com>
2021-11-13 18:13:23 +0000

    :construction:>>>>>:hammer_and_wrench:<<<<<:construction:

--- caliber/config/sendResult.py
+++ caliber/config/sendResult.py
@@ -2,32 +2,30 @@
 import os
 import json
 from collections import defaultdict
-# 해야할 부분 : 그들이 원한 방식으로 맞출 내용
 
 class SendResult:
-    # model 결과 받으면, 각 label별 cnt=1이 튜플 형식으로 쌓이게끔 작성
-    # label: c_9, count: 9
-    # answersheet_4_03_Caliber.json
-    def __init__(self, path=os.getcwd(), output=None):
+    def __init__(self, path=os.getcwd(),output=None):
+        '''output==predicted model'''
         self.path = os.path.join(path, "answersheet_4_03_Caliber.json")
         self.output = output
         self.result_form = self.Transfer()
-    def Transfer(self):
-        # tensor 내에 이미지 위치 정보도 있다는 가정하에 작성
-        # defaultdict로 작성함이 맞는건가...?
-        # self.file_name
-        # self.result
-        result_form = {"answer":[dict() for num in range(len(self.file_name))]}
-        # {
-        #     "answer":[{
-        #         "file_name":"t3_9999.jpg",
-        #         "result":[
-        #             {"label":"c_9", "count":"9"}
-        #         ]
-        #     }]
-        # }
+        
+    def Transfer(self, test_image):
+        '''test_image는 나중에 지우기!'''
+        # result_form = {"answer":[dict([("file_name",None), ("result", [])] for num in range(len(self.output))]}
+        # prediction, type : list --> prediction[0].items(), "labels" -> tensor.1d(float32)
+        result_form = {"answer":[dict([("file_name",None), ("result", [])]) for num in range(len(test_image))]}
+        for num, fname in enumerate(test_image):
+            result_form["answer"][num]["file_name"] = fname
+            
+            labels = [(0, 1), (1, 1), (2, 1), (3, 1), (3, 1)]
+            result = defaultdict(int)
+            for k, v in labels:
+                result[f'c_{str(k+1)}'] += v
+            sorted(result.items())
+            result_form["answer"][num]["result"] = [dict([("label",k), ("count",str(result.get(k)))]) for k in result.keys()]
         return result_form
     
     def FIN(self):
         with open(self.path, 'w') as jsf:
-            jsf.write(json.dumps(self.result_form, indent=4, Type=str))
+            jsf.write(json.dumps(self.result_form, indent=4))
--- caliber/main.py
+++ caliber/main.py
@@ -1,24 +1,44 @@
 import os
-from glob import glob as gb
+import sys
 import torch
-# from config.envinfos import EnvInfos    #, engine
-# from config.train import *
-# from config.models import * # CaliberM, CaliberOpt
-# references/detection/engine.py ... ././utils.py 
-# from engine import train_one_epoch, evaluate
-# import utils
+from glob import glob as gb
+from config.sendResult import SendResult
 
-test_path = '/home/agc2021/dataset'
-test_image = gb(f'{test_path}/t3_*.JPG')
-assert test_image
-
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-print(device)
-print(torch.cuda.device_count())
-# 몇 개가 걸리는지에 따라 병렬로 받을지는 조금 더 판 뒤에 해야함...
-# EnvInfos().memCheck()
-# model = CaliberM()
-# optimizer = CaliberOpt()
-
-# inference는 따로 .json으로 나와야 함.
-# from config.resultForm import Result
+try:
+    EF = ["test_path"]
+    test_path = '/home/agc2021/dataset'
+    test_image = gb(f'{test_path}/t3_*.JPG')
+    assert test_image
+    
+    EF.append("asset_path_path")
+    train_path = '/home/agc2021/assets'
+    train_image = gb(f'{train_path}/t3_*.JPG')
+    assert train_image
+    
+    EF.append("asset_path_os")
+    train_path = os.path.join(os.getcwd(), 'assets')
+    train_image = gb(f'{train_path}/t3_*.JPG')
+    assert train_image
+    
+    EF.append("torch_device")    
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    assert device
+    
+    EF.append("torch_gpu or parallel")
+    with torch.cuda.device(0):
+        if torch.cuda.device_count() >= 2:
+            x = torch.tensor([1., 2.]).to(torch.device('cuda:1'))
+            x = torch.tensor([1., 2.]).to(device)
+    assert x
+    
+    EF.append("model")
+    extension = 'pth'
+    num_classes = 7 + 1
+    option = 'inference'
+    
+    
+    EF.append("sendResult")
+    SendResult.FIN()
+    
+except:
+    print(EF[-1])

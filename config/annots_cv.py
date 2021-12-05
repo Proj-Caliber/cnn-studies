e8bab7ad9642e5c9aebf30f8b098c1c666ea8df5
AshbeeKim <ksb.forest@gmail.com>
2021-12-05 18:24:47 +0900

    :hammer_and_wrench:---baseline_v0.0.1---:hammer_and_wrench:

--- config/annots_cv.py
+++ config/annots_cv.py
@@ -1,22 +1,26 @@
 import cv2 as cv
-from PIL import Image
 import matplotlib.pyplot as plt
 
 class annotCats:
-    def __init__(self, image_path=None, label=None):
+    def __init__(self, image_path=None, label=None, xtickrange=100, ytickrange=100):
         self.colors = self.cvColor()
         self.label = label
         try:
-            self.image = Image.open(image_path)
+            self.image = cv.imread(image_path)
             self.image = cv.cvtColor(self.image, cv.COLOR_BGR2RGB)
+            self.H, self.W, self.C = self.image.size
         except:
             print("이미지 경로를 제대로 입력하세요.")
+        self.xtickrange = xtickrange
+        self.ytickrange = ytickrange
         
     def img2points(self, x, y, pcolor="RED", size=5, option=-1, check=True):
         if check:
             self.updated_img = self.image.copy()
-        plt.grid(color=self.colors["WHITE"], linestyle="dashdot", linewidth=.5)
+        plt.grid(color="white", linestyle="dashdot", linewidth=.5)
         cv.circle(self.updated_img, (x, y), size, self.colors[pcolor], option)
+        plt.xtick([num for num in range(0, self.W+1, self.xtickrange)])
+        plt.ytick([num for num in range(0, self.H+1, self.ytickrange)])
         plt.imshow(self.updated_img)
         if not check:
             return self.updated_img
@@ -24,8 +28,10 @@ class annotCats:
     def img2rectangle(self, x1, y1, x2, y2, pcolor="RED", check=True, rect_lwidth=2):
         if check:
             self.updated_img = self.image.copy()
-        plt.grid(color=self.colors["WHITE"], linestyle="dashdot", linewidth=.5)
+        plt.grid(color="white", linestyle="dashdot", linewidth=.5)
         cv.rectangle(self.updated_img, (x1, y1), (x2, y2), self.colors[pcolor], linewidth=rect_lwidth)
+        plt.xtick([num for num in range(0, self.W+1, self.xtickrange)])
+        plt.ytick([num for num in range(0, self.H+1, self.ytickrange)])
         plt.imshow(self.updated_img)
         if not check:
             return self.updated_img 
@@ -56,5 +62,17 @@ class annotCats:
         return colors
     
 # segmentation(polygon)은 따로 작성해야할 것 같음
-# class segPolygon:
-# 
+class segPolygon(annotCats):
+    def __init__(self, object_points=None):
+        super(segPolygon, self).__init__()
+        self.obj_points = object_points
+        
+        
+    def points2polygon(self):
+        img = self.image
+        ############################## 여기 추가 작성해야 함 ##################################
+        plt.xtick([num for num in range(0, self.W+1, self.xtickrange)])
+        plt.ytick([num for num in range(0, self.H+1, self.ytickrange)])
+        
+        plt.imshow(img)
+        

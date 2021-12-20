a167a507562d84532bf8c165ddc94e248a3ddd7c
AshbeeKim <co_m@gnu.ac.kr>
2021-12-20 13:58:17 +0000

    :hammer_and_wrench::truck::hammer_and_wrench:

--- main.py
+++ main.py
@@ -1,55 +1,56 @@
-import os
 import sys
+import argparse
 import torch
-from glob import glob as gb
-from config.sendResult import SendResult
-from config.model import CaliberM
 
-################################# 경로 확인되면 모델 작동되는 코드만 남기고 지워야 함 ###############################
-try:
-    # EF = ["test_path"]
-    # test_path = '/home/agc2021/dataset'
-    # test_image = gb(f'{test_path}/t3_*.JPG')
-    # assert test_image
-    
-    # EF.append("asset_path_path")
-    # train_path = '/home/agc2021/assets'
-    # train_image = gb(f'{train_path}/t3_*.JPG')
-    # assert train_image
-    
-    EF = ["asset_path"]
-    assets_path = os.path.join(os.getcwd(), 'assets')
-    taco_weights = gb(f'{assets_path}/taco/model*')
-    train_image = gb(f'{assets_path}/train/t3_*.JPG')
-    EF.append("taco")
-    assert taco_weights
-    EF.append("train")
-    assert train_image
-    
-    EF.append("torch_device")    
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    assert device
-    
-    EF.append("torch_gpu")
-    device = torch.device("cuda")
-    assert device
-    
-    EF.append("torch_parallel")
-    with torch.cuda.device(0):
-        if torch.cuda.device_count() >= 2:
-            x = torch.tensor([1., 2.]).to(torch.device('cuda:1'))
-            x = torch.tensor([1., 2.]).to(device)
-    assert x
-    
-    EF.append("model")
-    extension = 'pth'
-    num_classes = 7 + 1
-    option = 'inference'
-    
-    model = CaliberM()
-    
-    EF.append("sendResult")
-    SendResult().FIN()
-    
-except:
-    print(EF[-1])
+# try:
+EF = ['parser']
+# 1~main.py까지 다 작성 시, 걸리는 구간 확인용으로 assert
+parser = argparse.ArgumentParser()
+parser.add_argument('--epoch', type=int, default=5, help='')
+parser.add_argument('--batch', type=int, default=1, help='') 
+args = parser.parse_args()
+# parser.add_argument('--lr', type=, default=, help='') 
+# data_type? case?가 어떠한가, epoch, batch_size, 
+# model -> num_classes, option:init/update/inference, pretrained weight가 있는가.(extension:pt, pth)
+# argparse로 처리할 부분과 shell script config로 진행할 부분 고려해보기
+
+EF.append('device')
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+with torch.cuda.device(0):
+    if torch.cuda.device_count() >= 2:
+        x = torch.tensor([1., 2.]).to(torch.device('cuda:1'))
+        x = torch.tensor([1., 2.]).to(device)
+
+# parallel가능 여부에 따라, logic은 달라야 함.
+
+'''
+1. 데이터 불러와서 처리하는 모듈 : train(+ validation), test(what if...alias validation?)
+# google.colab
+from google.colab import drive
+ROOT = "/"
+from glob import glob as gb
+'''
+
+#     model = CaliberM()
+
+'''
+2. 모델(단순 detection, detect+segs, transforms(+ augmentation)
+# from models.model import CaliberM
+# model, loss, tensor에서 cuda사용. GPU memory불균형이 생길 수 있음(data parallel)
+nn.DataParallel or nn.DistributedDataParallel
+
+3. train or eval
+3_1. 학습만 했을 때, 어떤 기준을 가지고 가중치를 업데이트 할 것인지
+3_2. U-Net 적용? 미적용?
+
+4. 결과
+from models.sendResult import SendResult
+4_1. 추론 후, 결과 json 파일 형태로 return
+4_2. random한 n개의 추론 결과 시각화(format, gif? png? html?)
+'''
+
+#     EF.append("sendResult")
+#     SendResult().FIN()
+
+# except:
+#     print(EF[-1])
---
+++ main.sh
@@ -0,0 +1,7 @@
+# main.sh
+export CUDA_VISIBLE_DEVICES=1
+export BASE_DIR=/content/drive/MyDrive/Task/plastic-segmentation/Data
+
+# init
+python main.py
+# python main.py --basedir 

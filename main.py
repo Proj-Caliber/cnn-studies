cd32f2f5ae141bc8a29aa891f23a3e140651a567
AshbeeKim <co_m@gnu.ac.kr>
2021-12-20 15:35:39 +0000

    :hammer_and_wrench::truck::hammer_and_wrench:

--- main.py
+++ main.py
@@ -1,25 +1,30 @@
+# -*- python-mode -*-
+# -*- coding: utf-8 -*-
+import os
 import sys
 import argparse
 import torch
 
+os.environ["NUM_CLASSES"]
+# print(NUM_CLASSES)
 # try:
-EF = ['parser']
+# EF = ['parser']
 # 1~main.py까지 다 작성 시, 걸리는 구간 확인용으로 assert
-parser = argparse.ArgumentParser()
-parser.add_argument('--epoch', type=int, default=5, help='')
-parser.add_argument('--batch', type=int, default=1, help='') 
-args = parser.parse_args()
+# parser = argparse.ArgumentParser()
+# parser.add_argument('--epoch', type=int, default=5, help='')
+# parser.add_argument('--batch', type=int, default=1, help='') 
+# args = parser.parse_args()
 # parser.add_argument('--lr', type=, default=, help='') 
 # data_type? case?가 어떠한가, epoch, batch_size, 
 # model -> num_classes, option:init/update/inference, pretrained weight가 있는가.(extension:pt, pth)
 # argparse로 처리할 부분과 shell script config로 진행할 부분 고려해보기
 
-EF.append('device')
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-with torch.cuda.device(0):
-    if torch.cuda.device_count() >= 2:
-        x = torch.tensor([1., 2.]).to(torch.device('cuda:1'))
-        x = torch.tensor([1., 2.]).to(device)
+# EF.append('device')
+# device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+# with torch.cuda.device(0):
+#     if torch.cuda.device_count() >= 2:
+#         x = torch.tensor([1., 2.]).to(torch.device('cuda:1'))
+#         x = torch.tensor([1., 2.]).to(device)
 
 # parallel가능 여부에 따라, logic은 달라야 함.
 
--- main.sh
+++ main.sh
@@ -1,7 +1,11 @@
 # main.sh
 export CUDA_VISIBLE_DEVICES=1
 export BASE_DIR=/content/drive/MyDrive/Task/plastic-segmentation/Data
+export NUM_CLASSES=5
 
 # init
 python main.py
-# python main.py --basedir 
+# python main.py --
+# python main.py --
+# python main.py --
+# python main.py --
